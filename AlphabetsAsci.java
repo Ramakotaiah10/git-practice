fd3dc1572b69950bfd50c02359d67e96d613cf28
Ramakotaiah10 <ramakotaiah10@gmail.com>
2022-01-04 11:06:30 +0530

    First commit

---
+++ AlphabetsAsci.java
@@ -0,0 +1,10 @@
+class AlphabetsAsci 
+{
+	public static void main(String[] args) 
+	{   for(char ch=65; ch<91; ch++)
+		{
+            // int code=(int) ch;
+		System.out.println(ch+" : "+code);
+	   } 
+	}
+}
---
+++ Test.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;	
+class Test
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the value of n");
+n=sc.nextInt();
+int count=0;
+while (i<=n)
+{
+	if (n%i==0)
+	{
+		count++;
+	}
+	i++;
+}
+System.out.print("My name is Dell");
+
+
+
+}
+}
